34a3d46ea59443ead61a2c025d54c25c359e76e9
wlam135 <wlam135@naver.com>
2025-05-21 22:15:42 +0900

    IntersectRectangle

---
+++ src/ch04/IntersectRectangle_2.java
@@ -0,0 +1,17 @@
+package ch04;
+
+// 두 사각형이 X,Y축과 평행하지 않는다면,
+// 어떻게 겹치는지 확인할까?
+// 고민해보기
+public class IntersectRectangle_2 {
+    public static class Rectangle {
+        private final int x, y, width, heigh;
+
+        public Rectangle(int x, int y, int width, int heigh) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.heigh = heigh;
+        }
+    }
+}
